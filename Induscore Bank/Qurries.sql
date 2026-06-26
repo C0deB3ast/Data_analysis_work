a9579eda4b9efa162ba28cd15c13f48c1b78d5e4
C0deB3ast <37912nishu.nk@gmail.com>
2026-06-26 06:22:50 +0000

    What is the total transaction amount of each branch then find only those branch whoes total transactions is higher than 5 lakh.

--- Induscore Bank/Qurries.sql
+++ Induscore Bank/Qurries.sql
@@ -21,7 +21,7 @@ ORDER BY
 total_loan DESC;*/
 
 /*Find the customers who's loan amount is higher than overall average loan amount*/
-SELECT
+/*SELECT
 cst.name,
 cst.segment,
 ln.amount
@@ -29,4 +29,22 @@ From customers cst
 Left join loan ln on cst.customer_id = ln.customer_id
 Where amount > (SELECT
 Avg(amount) as Avg_loan
-From loan)
+From loan)*/
+
+/*What is the total transaction amount of each branch then find only those branch whoes total transactions is higher than 5 lakh.*/
+WITH more_5lk AS(
+Select
+br.branch_name,
+Sum(trn.amount) AS total_amount 
+From transactions trn
+Left join Accounts acc ON trn.account_id = acc.account_id
+LEFT JOIN customers cst ON acc.customer_id = cst.customer_id
+LEFT JOIN branches br ON cst.branch_id = br.branch_id
+WHERE trn.amount IS NOT NULL
+GROUP BY 
+br.branch_name
+)
+
+SELECT *
+From more_5lk
+WHERE total_amount > 500000
